63b43a53577d4d7701bbc01803e354feebb74717
Matt Evans <mattevans98@gmail.com>
2019-12-03 12:27:09 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(function () {
+    $("#splash_btn").on("click", function () {
+        $("#splash_screen").css("display", "none");
+        $("#main_content").css("display", "block");
+    });
+});
